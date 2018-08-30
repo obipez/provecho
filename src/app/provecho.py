d6fc3399dd2b34ef3ff9f389f96988e515748012
deimosmorte <38267196+deimosmorte@users.noreply.github.com>
2018-08-30 01:28:42 -0500

    Updating Flask app

--- src/app/provecho.py
+++ src/app/provecho.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/hello")
+def hello():
+    return "Hello World!”
 
 if __name__ == "__main__":
-app.run(host='0.0.0.0', port=5000, debug=True)
+app.run()
