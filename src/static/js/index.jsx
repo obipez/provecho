cd95c95a87938e4aa8008bcf8384f1c02d1caa8d
deimosmorte <38267196+deimosmorte@users.noreply.github.com>
2018-08-30 01:26:40 -0500

    Updating index to include App component

--- src/static/js/index.jsx
+++ src/static/js/index.jsx
@@ -1,1 +1,6 @@
-alert(“Hello World!”);
+// index.jsx
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("content"));
