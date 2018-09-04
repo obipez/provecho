3d63bec415997d8bf7aa580d61e64dda161534e1
Cory Comer <comercory@gmail.com>
2018-09-03 22:28:33 -0500

    Adding updates to the python app

--- src/app/provecho.py
+++ src/app/provecho.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route("/hello")
 def hello():
-    return "Hello World!”
+    return "Hello World!"
 
 if __name__ == "__main__":
-app.run()
+	app.run(host='0.0.0.0', port=5000, debug=True)
